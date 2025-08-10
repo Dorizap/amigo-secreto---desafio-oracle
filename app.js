18a7dd716a88d5c76cf64ee6ec4b21de58196a78
Dorizap <doriannyszapata20@gmail.com>
2025-08-10 18:16:05 -0500

    empece de nuevo

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-let listaIngreso = [];
 
-function agregarAmigo() {
-    let datoUsuario = document.getElementById('amigo').value.trim();
-
-    if (datoUsuario == "") {
-        alert("Vuelve a ingresar un nombre:)");
-        return; 
-    }
-
-    listaIngreso.push(datoUsuario);
-    console.log(listaIngreso);
-}
