35b61e4ab49b18fd0f57a579171ebfcc22843361
Dorizap <doriannyszapata20@gmail.com>
2025-08-10 18:04:39 -0500

    Agregue la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 let listaIngreso = [];
 
+function agregarAmigo() {
+    let datoUsuario = document.getElementById('amigo').value.trim();
+
+    if (datoUsuario == "") {
+        alert("Vuelve a ingresar un nombre:)");
+        return; 
+    }
+
+    listaIngreso.push(datoUsuario);
+    console.log(listaIngreso);
+}
