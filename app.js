c5d2af7c0577c9c4e40c5e2808de10d57e799996
Dorizap <doriannyszapata20@gmail.com>
2025-08-11 12:43:51 -0500

    Update app.js
    a;adiendo el math.floor y math.random

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ function sortearAmigo() {
     if (amigos.length === 0) { // para comprobar q la lista no este vacia
         alert("No hay amigos para sortear. Agrega al menos uno."); // mensaje por si esta vacia
         return; //aca detiene mientras siga vacia
-        
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length); // crear el math para el indice aleatorio del arreglo
     }
 }
