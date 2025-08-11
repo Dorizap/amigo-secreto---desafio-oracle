1be02fb90f9d6ecd05d3e183bb216d9740877fc5
Dorizap <doriannyszapata20@gmail.com>
2025-08-10 21:34:37 -0500

    Update app.js
    creando todo y testeando directamente con VS code (se me hace mas facil)

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = [];
+
+function agregarAmigo() {
+    let datoUsuario = document.getElementById('amigo').value.trim();
+    if (datoUsuario === '') {
+        alert('Por favor inserta un nombre');
+        return; 
+    } else {
+        amigos.push(datoUsuario);
+    }
+
+    console.log(datoUsuario);
+    console.log(amigos);
+    vaciarBox();
+}
